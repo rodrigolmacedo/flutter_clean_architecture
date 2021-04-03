cbe1aa294f3944a3825b54ea8504c7b25c74ba75
Rodrigo <7695045+rodrigolmacedo@users.noreply.github.com>
2021-04-03 19:23:19 -0300

    infra

---
+++ test/modules/search/infra/repositories/search_repository_test.dart
@@ -0,0 +1,31 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_clean_architecture/modules/search/domain/errors/errors.dart';
+import 'package:flutter_clean_architecture/modules/search/infra/datasources/search_datasource.dart';
+import 'package:flutter_clean_architecture/modules/search/infra/models/result_search_model.dart';
+import 'package:flutter_clean_architecture/modules/search/infra/repositories/search_repository_impl.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+class SearchDataSourceMock extends Mock implements SearchDatasource {}
+
+main() {
+  final datasource = SearchDataSourceMock();
+  final repository = SearchRepositoryImpl(datasource);
+
+  test('deve retornar uma lista de resultSearch', () async {
+    when(datasource.getSearch(any))
+        .thenAnswer((realInvocation) async => <ResultSearchModel>[]);
+
+    final result = await repository.search("searchText");
+
+    expect(result | null, isA<List<ResultSearchModel>>());
+  });
+
+  test('deve retornar um DatasourceError se o datasouce falhar', () async {
+    when(datasource.getSearch(any)).thenThrow(Exception());
+
+    final result = await repository.search("searchText");
+
+    expect(result.fold(id, id), isA<DatasourceError>());
+  });
+}
